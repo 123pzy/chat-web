402a6d7875c63ef94fb058f5aae77737b0e602ed
panpan <zeyupan@163.com>
2023-07-31 11:15:14 +0800

    增加删除funcBoard的鼠标一如移出的显示效果

--- src/components/HomeFuncBoard.vue
+++ src/components/HomeFuncBoard.vue
@@ -1,25 +1,26 @@
 <script setup>
-import sprite from '../assets/icon/sprite.svg';
-import sprite2 from '../assets/icon/sprite2.svg';
-import { useFuncBroad } from '../stores/funcBoard.js';
-import { useFuncCate } from '../stores/funcCate';
-import { useStyle } from '../stores/style';
-import { storeToRefs } from 'pinia';
-import { ElMessage, ElMessageBox } from 'element-plus';
-import { setUsersFuncBoard, deleteUsersFuncBoard } from '../api/request';
-import { onMounted } from 'vue';
-import { useRouter } from 'vue-router';
+import sprite from "../assets/icon/sprite.svg";
+import sprite2 from "../assets/icon/sprite2.svg";
+import { useFuncBroad } from "../stores/funcBoard.js";
+import { useFuncCate } from "../stores/funcCate";
+import { useStyle } from "../stores/style";
+import { storeToRefs } from "pinia";
+import { ElMessage, ElMessageBox } from "element-plus";
+import { setUsersFuncBoard, deleteUsersFuncBoard } from "../api/request";
+import { onMounted, ref } from "vue";
+import { useRouter } from "vue-router";
 
 const style = useStyle();
 const { fontColor, theme } = storeToRefs(style);
 const funcBoard = useFuncBroad();
 const funcCate = useFuncCate();
-const username = localStorage.getItem('username');
+const username = localStorage.getItem("username");
 const router = useRouter();
+const deleteBtn = ref();
 
 // 点击板块跳转chatpage组件
 function handleClick(e, route) {
-  if (e.target.tagName != 'IMG') {
+  if (e.target.tagName != "IMG") {
     router.push(route);
   }
   console.log(e.target.tagName);
@@ -28,12 +29,12 @@ function handleClick(e, route) {
 // element弹出输入框,用于自定义添加funcBoard板块
 const open = () => {
   ElMessageBox.prompt(
-    '请输入你要chatGPT扮演一个什么样的角色，比如你想让它做你的英语老师，检查你的英文是否包含语法错误，你就可以说:你现在是我的英文老师，检查我发给你的每段英文中是否包含语法错误。关于chatgpt扮演的角色描述的越详细越好~',
-    '自定义chatgpt',
+    "请输入你要chatGPT扮演一个什么样的角色，比如你想让它做你的英语老师，检查你的英文是否包含语法错误，你就可以说:你现在是我的英文老师，检查我发给你的每段英文中是否包含语法错误。关于chatgpt扮演的角色描述的越详细越好~",
+    "自定义chatgpt",
     {
-      confirmButtonText: '确认',
-      cancelButtonText: '取消',
-      inputErrorMessage: '输入的不对哦~',
+      confirmButtonText: "确认",
+      cancelButtonText: "取消",
+      inputErrorMessage: "输入的不对哦~",
       lockScroll: false,
     }
   )
@@ -41,10 +42,10 @@ const open = () => {
       if (value) {
         const message = value;
         setTimeout(() => {
-          ElMessageBox.prompt('请给你的自定义板块起个名字', '输入板块名', {
-            confirmButtonText: '确认',
-            cancelButtonText: '取消',
-            inputErrorMessage: '输入的不对哦~',
+          ElMessageBox.prompt("请给你的自定义板块起个名字", "输入板块名", {
+            confirmButtonText: "确认",
+            cancelButtonText: "取消",
+            inputErrorMessage: "输入的不对哦~",
             lockScroll: false,
           })
             .then(async ({ value }) => {
@@ -52,8 +53,8 @@ const open = () => {
                 const func = {
                   id: funcBoard.funcBoard.length + 1,
                   func: value,
-                  icon: 'all',
-                  label: '',
+                  icon: "all",
+                  label: "",
                   route: `chat_your-maked-${funcBoard.funcBoard.length + 1}`,
                   message: `${message}`,
                 };
@@ -61,56 +62,72 @@ const open = () => {
                 // 添加新的funcBoard到数据库
                 await setUsersFuncBoard({ func, username });
                 ElMessage({
-                  type: 'success',
+                  type: "success",
                   message: `自定义功能成功，快去试试吧！`,
                 });
               }
             })
             .catch(() => {
               ElMessage({
-                type: 'info',
-                message: '自定义功能很好玩的~再试试吧~',
+                type: "info",
+                message: "自定义功能很好玩的~再试试吧~",
               });
             });
         }, 500);
         ElMessage({
-          type: 'success',
+          type: "success",
           message: `下面输入你自定义板块的名字吧`,
         });
       }
     })
     .catch(() => {
       ElMessage({
-        type: 'info',
-        message: '自定义功能很好玩的~再试试吧~',
+        type: "info",
+        message: "自定义功能很好玩的~再试试吧~",
       });
     });
 };
 // 删除funcBoard
 async function deleteFuncBoard(id) {
-  ElMessageBox.confirm('确认要删掉该板块吗?', '确认删掉？', {
-    confirmButtonText: '确认',
-    cancelButtonText: '取消',
-    type: '确认要删掉吗？',
+  ElMessageBox.confirm("确认要删掉该板块吗?", "确认删掉？", {
+    confirmButtonText: "确认",
+    cancelButtonText: "取消",
+    type: "确认要删掉吗？",
+    lockScroll: false, // 控制弹出框之后页面不要抖动
   })
     .then(async () => {
       const data = { id, username };
       const res = await deleteUsersFuncBoard(data);
       funcBoard.getFuncBoard(username);
       ElMessage({
-        type: 'success',
+        type: "success",
         message: res.data.message,
       });
     })
     .catch(() => {
       ElMessage({
-        type: 'info',
-        message: '已取消删除',
+        type: "info",
+        message: "已取消删除",
       });
     });
 }
+
+// funcBoard的鼠标移入事件
+function mouseEnter(id) {
+  const btn = deleteBtn.value.find((item) => {
+    return item.id == id;
+  });
+  btn.style.display = "block";
+}
+// 鼠标移出事件
+function mouseLeave(id) {
+  const btn = deleteBtn.value.find((item) => {
+    return item.id == id;
+  });
+  btn.style.display = "none";
+}
 onMounted(async () => {
-  const username = localStorage.getItem('username');
+  const username = localStorage.getItem("username");
   funcBoard.getFuncBoard(username);
 });
 </script>
@@ -121,6 +138,8 @@ onMounted(async () => {
       v-for="board in funcBoard.funcBoard"
       v-show="funcCate.cate === '全部' ? true : board.label === funcCate.cate"
       @click="handleClick($event, board.route)"
+      @mouseenter="mouseEnter(board.id)"
+      @mouseleave="mouseLeave(board.id)"
       class="board-box"
     >
       <svg class="funcboard_icon">
@@ -133,9 +152,10 @@ onMounted(async () => {
       </svg>
       <img
         src="../assets/icon/deleteBtn_black.svg"
-        alt=""
         class="btn-detele"
         @click="deleteFuncBoard(board.id)"
+        :id="board.id"
+        ref="deleteBtn"
       />
       <div class="func-name">{{ board.func }}</div>
     </div>
@@ -198,6 +218,7 @@ $font-color: v-bind(fontColor);
   width: 48px;
 }
 .btn-detele {
+  display: none;
   height: 25px;
   width: 25px;
   position: absolute;
